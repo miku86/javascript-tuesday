33333a0c0be1ee0d4a9639da6a447d218e1e8c0b
miku86 <miku86coding@gmail.com>
2020-01-28 14:25:58 +0100

    Add code for v1

---
+++ V1/script.js
@@ -0,0 +1,13 @@
+d3.select("h1")
+  .transition()
+  .style("color", "purple")
+
+const myData = [10, 20, 30, 40, 50];
+
+d3.select(".chart01")
+  .selectAll("div")
+  .data(myData)
+  .enter()
+  .append("div")
+  .text((value) => value)
+  .style("width", (value) => `${value * 3}px`);
