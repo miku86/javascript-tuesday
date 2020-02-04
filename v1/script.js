2c12ef3952ccecdc75970c9c0ede360f14ba89ab
miku86 <miku86coding@gmail.com>
2020-02-04 15:26:43 +0100

    Remove transition and add text

--- v1/script.js
+++ v1/script.js
@@ -1,13 +1,13 @@
 d3.select("h1")
-  .transition()
   .style("color", "purple")
+  .text("D3 for Pros");
 
 const myData = [10, 20, 30, 40, 50];
 
 d3.select(".chart01")
   .selectAll("div")
   .data(myData)
-  .enter()
-  .append("div")
-  .text((value) => value)
-  .style("width", (value) => `${value * 3}px`);
+    .enter()
+    .append("div")
+    .text((value) => value)
+    .style("width", (value) => `${value * 3}px`);
