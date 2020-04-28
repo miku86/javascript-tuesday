1a84efe818adc1e556b0a52637b8d8ec6bdf5a35
miku86 <miku86coding@gmail.com>
2020-04-28 21:10:46 +0200

    Add functionality to submit new player by clicking Enter

--- v4/script.js
+++ v4/script.js
@@ -1,5 +1,18 @@
 const app = document.querySelector(".app")
 
+function createForm() {
+  const customForm = document.createElement("form")
+  customForm.className = "form-add-player"
+  app.appendChild(customForm)
+
+  const myForm = document.querySelector(".form-add-player")
+  const myInputField = createInputField("New Player Name", "input-player-name")
+  myForm.appendChild(myInputField)
+
+  const myButton = createButton("Add Player", "add-player")
+  myForm.appendChild(myButton)
+}
+
 function createButton(buttonText, buttonId) {
   const customButton = document.createElement("button")
   customButton.textContent = buttonText
@@ -22,18 +35,21 @@ function createDiv(newClass) {
   app.appendChild(customDiv)
 }
 
-createInputField("New Player Name", "input-player-name")
-createButton("Add Player", "add-player")
+createForm()
+
 createDiv("players")
 
-let amountOfPlayers = 2
+let amountOfPlayers = 0
 const playerInputField = document.querySelector(".input-player-name")
-const addUserButton = document.querySelector("#add-player")
 const playersContainer = document.querySelector(".players")
 
 let playerNames = []
 
-addUserButton.addEventListener("click", () => {
+const addUserForm = document.querySelector(".form-add-player")
+
+addUserForm.addEventListener("submit", (event) => {
+  event.preventDefault()
+
   const player = document.createElement("p")
   player.textContent = playerInputField.value
   const newPlayer = {
