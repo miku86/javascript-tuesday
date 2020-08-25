9b6a36b0b2e91b77c2392867f72217ae13e65db3
miku86 <miku86coding@gmail.com>
2020-08-25 16:46:31 +0200

    Add basics exercises about data

---
+++ bonus/basics-daten.js
@@ -0,0 +1,91 @@
+/*
+ * DATENTYPEN (kleinste Informationseinheit)
+ * - abspeichern einer Information
+ * - vor Schreiben nachdenken, welcher Datentyp/Datenstruktur geeignet ist
+ */
+
+// String
+let myString;
+
+// Number
+let myNumber;
+
+// Boolean
+let myBoolean;
+
+/////////////////////////////////////////////
+
+/*
+ * DATENSTRUKTUREN (Informationen strukturieren)
+ * - abspeichern mehrerer Informationen
+ */
+
+// Array
+let myArray;
+
+// Object
+let myObject;
+
+/////////////////////////////////////////////
+
+/*
+ * Aufgabe 1: Daten speichern
+ *
+ * Speichere folgende Daten passend ab:
+ */
+
+// 1.1 dein Name
+
+// 1.2 dein Alter
+
+// 1.3 ob du Bayern-Fan bist
+
+// 1.4 dein Wohnort
+
+// 1.5 dein Name und dein Alter zusammen
+
+// 1.6 zwei Zahlen zusammen
+
+// 1.7 dein Name und ob du Bayern-Fan bist
+
+// 1.8 #5 und #6 zusammen
+
+/////////////////////////////////////////////
+
+/*
+ * Aufgabe 2: Daten lesen
+ *
+ * Lies folgende Daten aus:
+ */
+
+// 2.1 dein Alter aus 1.2
+
+// 2.2 dein Alter aus 1.5
+
+// 2.3 irgendeine Zahl
+
+// 2.4 dein Name aus 1.8
+
+// 2.5 ob du Bayern-Fan bist
+
+/////////////////////////////////////////////
+
+/*
+ * Aufgabe 3: Daten ändern
+ *
+ * Ändere folgende Daten:
+ */
+
+// 3.1 dein Name aus 1.2 in "Hans"
+
+// 3.2 ob du Bayern-Fan bist aus 1.3 ins Gegenteil
+
+// 3.3 die zweite Zahl aus 1.6
+
+// 3.4 dein Name aus 1.7 in "Fritz"
+
+// 3.5 verdopple die Zahlen aus 1.6 und speichere sie
+
+// 3.6 filtere die Zahlen aus 1.6, die kleiner sind als 5
+
+/////////////////////////////////////////////
