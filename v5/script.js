375ebe6ac9e6527f09a9ab0f429b5b1d061a3d76
miku86 <git@miku86.com>
2021-02-16 20:41:13 +0100

    Create view to input player

--- v5/script.js
+++ v5/script.js
@@ -13,11 +13,89 @@ const model = {
     this.playersData.push(newPlayer);
   },
 
-  // Inhalte von playersData entfernen
-
   // Inhalte von playersData ändern
 };
 
 const view = {
-  // playersData anzeigen
+  displayNewPlayerView() {
+    this.generateNewPlayerElements();
+  },
+
+  generateNewPlayerElements() {
+    const app = document.querySelector(".app");
+    const customForm = document.createElement("form");
+    customForm.className = "form-add-player";
+    app.appendChild(customForm);
+
+    const myForm = document.querySelector(".form-add-player");
+
+    const myInputField = this.createInputField(
+      "New Player Name",
+      "input-player-name",
+      "name" // for dummy data
+    );
+    myForm.appendChild(myInputField);
+
+    const myButton = this.createButton("Add Player", "add-player");
+    myForm.appendChild(myButton);
+  },
+
+  createInputField(inputPlaceholder, inputClassname, type) {
+    const playerInput = document.createElement("input");
+    playerInput.type = "text";
+    playerInput.className = inputClassname;
+    playerInput.placeholder = inputPlaceholder;
+    playerInput.autofocus = true;
+    playerInput.value =
+      type === "name" ? utils.randomName() : utils.randomNumber();
+    return playerInput;
+  },
+
+  createButton(buttonText, buttonId) {
+    const customButton = document.createElement("button");
+    customButton.textContent = buttonText;
+    customButton.id = buttonId;
+    return customButton;
+  },
 };
+
+const controller = {
+  createNewPlayer() {
+    // suche add player button
+
+    // lies spielername aus input feld aus
+
+    // speicher inhalte in model
+    model.createNewPlayer();
+
+    // zeige name an - view.???
+
+    // leere input feld
+  },
+};
+
+const utils = {
+  randomName() {
+    const names = [
+      "Adolf",
+      "Alfred",
+      "Bertram",
+      "Fridolin",
+      "Fritz",
+      "Hans",
+      "Horst",
+      "Max",
+      "Peter",
+    ];
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    return randomName;
+  },
+
+  randomNumber() {
+    const MIN = 100;
+    const MAX = 500;
+    return Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
+  },
+};
+
+view.displayNewPlayerView();
