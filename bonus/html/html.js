ed16419d3efba89185c452b63360f0657225ba48
To5ch <schneider.t.simmozheim@freenet.de>
2021-10-26 20:28:39 +0200

    Add button to change backgroundcolour

--- bonus/html/html.js
+++ bonus/html/html.js
@@ -1,18 +1,60 @@
-function createListItem(text) {
-  const li = document.createElement("li");
-  li.textContent = text;
-  ul.appendChild(li);
+//<!-- Ziel: button mit text erstellen, der auf knopfdruck hintergrundfarbe der Seite ändert-->
+//<!-- <button>Umschalten</button> -->
+// Erstelle Button
+const button = document.createElement("button")
+
+// Füge einen Text zum Button hinzu
+button.textContent = "Farbe ändern"
+
+// Hänge Button an
+const h1 = document.querySelector("h1")
+h1.after(button)
+
+// Clickaction hinzufügen
+button.addEventListener("click",changeBackgroundColour)
+
+// Wenn geklickt Hintergrundfarbe ändern
+function changeBackgroundColour() {
+  // Finde den Body
+  const body = document.querySelector("body")
+
+  // Füge dem Body einen Backgroundstyle hinzu
+  // Prüfe vorhandene Hintergrundfarbe "grün"
+  if (body.style.backgroundColor === "green") {
+    body.style.backgroundColor ="red"
+  } else {
+    body.style.backgroundColor = "green"
+  }
+  // Wenn ja, wechsele Hintergrunt auf rot
+  // wenn nein wechsle Hintergund auf grün
+  
+
+
+  //
+
 }
 
-const new_list = document.createElement("ul");
 
-const h1 = document.querySelector("h1");
 
-h1.after(new_list);
 
-const ul = document.querySelector("ul");
-createListItem("Kekse");
-createListItem("Bier");
+
+
+
+// function createListItem(text) {
+//   const li = document.createElement("li");
+//   li.textContent = text;
+//   ul.appendChild(li);
+// }
+
+// const new_list = document.createElement("ul");
+
+// const h1 = document.querySelector("h1");
+
+// h1.after(new_list);
+
+// const ul = document.querySelector("ul");
+// createListItem("Kekse");
+// createListItem("Bier");
 
 // function createBoxAndAppend(text) {
 //   // 1. div erstellen
@@ -30,3 +72,4 @@ createListItem("Bier");
 
 // createBoxAndAppend("Box 1");
 // createBoxAndAppend("Box 2");
+
