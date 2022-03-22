2fcb5014c96eb5286ff806cfc18c0468c35a23e7
To5ch <schneider.t.simmozheim@freenet.de>
2022-03-22 20:59:19 +0100

    learn node.js

---
+++ nodejs/addieren.test.js
@@ -0,0 +1,21 @@
+test("1+1=2",()=>{
+    expect(1).toBe(2)
+})
+
+test("1+a=false",()=>{
+    expect(1).toBe(false)
+})
+
+test("1=false",()=>{
+    expect(1).toBe(false)
+})
+
+test("'1'+'1'=2",()=>{
+    expect(1).toBe(2)
+})
+
+test("nichts eingegeben=false",()=>{
+    expect(1).toBe(false)
+})
+
+
